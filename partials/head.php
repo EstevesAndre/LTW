3307280d758cacec6afb5ee7dbba5be309433c61
EstevesAndre <up201606673@fe.up.pt>
2018-11-11 21:30:57 +0000

    deleted images folder and erased git conflicts

--- partials/head.php
+++ partials/head.php
@@ -1,22 +1,12 @@
-<<<<<<< HEAD:partials/head.php
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Prayers R Us<?php echo($subtitle) ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="../styles/stylesheet.css" />
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
-    <script src="../scripts/script.js"></script>
-</head>
-=======
 <!DOCTYPE <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Antero Total | Categories</title>
+        <title>Prayers R Us<?php echo($subtitle) ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" media="screen" href="../styles/stylesheet.css" />
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
         <script src="../scripts/script.js"></script>
     </head>
     <body>
@@ -82,4 +72,3 @@
         </header>
     </body>
 </html>
->>>>>>> d0cca3974ca67f6e8588bb2cd7a3bb28b796b088:views/categories.html
