7d9c4c088f5e184240985db71e87596888e7a165
neto22 <pneto.98@gmail.com>
2018-11-12 10:52:59 +0000

    index done

--- views/index.php
+++ views/index.php
@@ -5,8 +5,28 @@
 <body>
     <?php include'../partials/nav-bar.php';?>
     <div class="main">
-
+        <div class="title">
+            <p>Prayers R Us</p>
+        </div>
+        <div class="subtitle">
+            <p>Always remember: A prayer a day keeps the devil away</p>
+            <!-- <p>A collection of prayers meant to please everyone</p> -->
+        </div>
+        <svg width='100%' height='100%' viewBox="0 0 100 80" preserveAspectRatio="none" style="fill:#9c9e9e">
+            <polygon points="0,62.5 0,100 25,100 25,67.5" />
+            <polygon points="25,100 25,67.5 75,60 100,65 100,100" /> />
+        </svg>
+        <svg width='100%' height='100%' viewBox="0 0 100 80" preserveAspectRatio="none" style="fill:#f1f1f1">
+            <polygon points="0,65 0,100 25,100 25,70" />
+            <polygon points="25,100 25,70 75,62.5 100,67.5 100,100" />
+        </svg>
     </div>
+    <div class="section">
+        <p class="title">Every day is a happy day when you pray</p>
+        <p class="text">Prayer is an invocation or act that seeks to activate a rapport with an object of worship, typically a deity, through deliberate communication. In the narrow sense, the term refers to an act of supplication or intercession directed towards a deity, or a deified ancestor.</p>
+        <p class="text">More generally, prayer can also have the purpose of thanksgiving or praise, and in comparative religion is closely associated with more abstract forms of meditation and with charms or spells.</p>
+        <a class="button" href="./fresh.php">Come pray with us!</a>
+    <div>    
 </body>
 
 </html>
