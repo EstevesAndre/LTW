a63755ae3a2661f306913372506138a08ff9be81
EstevesAndre <up201606673@fe.up.pt>
2018-11-29 23:46:30 +0000

    added actions, login logout and signup functions

---
+++ actions/login.php
@@ -0,0 +1,21 @@
+<?php
+    include_once('../includes/session.php');
+    include_once('../database/db_checkUser.php');
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if(isLoginCorrect($username, $password))
+    {
+        $_SESSION['username'] = $username;
+        $_SESSION['messages'][] = array('type' => 'success', 'content' => 'Logged in successfully!');
+
+        header('Location: ../pages/mainMenu.php');
+    }
+    else
+    {
+        $_SESSION['messages'][] = array('type' => 'error', 'content' => 'Login failed!');
+        header('Location: ../pages/login.php');
+    }
+
+?>
