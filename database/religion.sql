83df494276a49d9bf5b900f67a2969e761bf6d21
EstevesAndre <up201606673@fe.up.pt>
2018-12-01 19:48:49 +0000

    changed database

--- database/religion.sql
+++ database/religion.sql
@@ -1,131 +1,138 @@
-CREATE TABLE channel (
+-- Table: Channel
+DROP TABLE IF EXISTS Channel;
+
+CREATE TABLE Channel (
     id INTEGER PRIMARY KEY,
     cType VARCHAR NOT NULL
 );
 
-CREATE TABLE userLikesChannel (
-    id_channel INTEGER REFERENCES channel(id),
-    username_user VARCHAR REFERENCES user(username)
+-- Table: UserLikesChannel
+DROP TABLE IF EXISTS UserLikesChannel;
+
+CREATE TABLE UserLikesChannel (
+    id_Channel INTEGER REFERENCES Channel(id),
+    username_user VARCHAR REFERENCES User(username)
 );
 
-CREATE TABLE user (
+-- Table: User
+DROP TABLE IF EXISTS User;
+
+CREATE TABLE User (
     username VARCHAR PRIMARY KEY,
+    email VARCHAR UNIQUE,
     password VARCHAR NOT NULL
 );
 
-CREATE TABLE publication (
+-- Table: Publication
+DROP TABLE IF EXISTS Publication;
+
+CREATE TABLE Publication (
     id INTEGER PRIMARY KEY,
-    type CHAR NOT NULL,
-    title VARCHAR,
+    username VARCHAR REFERENCES User,
     published INTEGER, -- date when this was published
     tags VARCHAR NOT NULL, -- comma separated tags
-    username VARCHAR REFERENCES user,
-    introduction VARCHAR,
-    fulltext VARCHAR NOT NULL,
-    image_path VARCHAR,
-    upVote INTEGER,
-    downVote INTEGER
+    title VARCHAR,
+    fulltext VARCHAR NOT NULL
 );
 
-CREATE TABLE comment (
+-- Table: Comment
+DROP TABLE IF EXISTS Comment;
+
+CREATE TABLE Comment (
     id INTEGER PRIMARY KEY,
-    type CHAR NOT NULL,
-    news_id INTEGER REFERENCES publication,
-    username VARCHAR REFERENCES user,
-    comment_to INTEGER NULL REFERENCES comment(id), -- self relationship
+    username VARCHAR REFERENCES User,
+    publication_id INTEGER REFERENCES Publication(id),
+    comment_id INTEGER NULL REFERENCES Comment(id), -- self relationship
     published INTEGER, -- date when this was published
     tags VARCHAR, -- comma separated tags
-    text VARCHAR NOT NULL,
-    upVote INTEGER,
-    downVote INTEGER
+    text VARCHAR NOT NULL
 );
 
-CREATE TABLE voted(
+-- Table: Voted
+DROP TABLE IF EXISTS Voted;
+
+CREATE TABLE Voted(
     id INTEGER PRIMARY KEY,
     type CHAR NOT NULL,
-    username VARCHAR REFERENCES user, 
-    id_publication INTEGER NULL REFERENCES publication(id),
-    id_comment INTEGER NULL REFERENCES comment(id)
+    username VARCHAR REFERENCES User, 
+    publication_id INTEGER NULL REFERENCES Publication(id),
+    comment_id INTEGER NULL REFERENCES Comment(id)
 );
 
-INSERT INTO channel VALUES (NULL, 'General');
-INSERT INTO channel VALUES (NULL, 'Christianity');
-INSERT INTO channel VALUES (NULL, 'Islamism');
-INSERT INTO channel VALUES (NULL, 'Hinduism');
-INSERT INTO channel VALUES (NULL, 'Buddhism');
-INSERT INTO channel VALUES (NULL, 'Baháí Faith');
-INSERT INTO channel VALUES (NULL, 'Jainism');
-INSERT INTO channel VALUES (NULL, 'Judaism');
-INSERT INTO channel VALUES (NULL, 'Paganism');
-INSERT INTO channel VALUES (NULL, 'Shintoism');
-INSERT INTO channel VALUES (NULL, 'Sikhism');
-INSERT INTO channel VALUES (NULL, 'Taoism');
-INSERT INTO channel VALUES (NULL, 'Folk channels');
-INSERT INTO channel VALUES (NULL, 'Irchannel');
+INSERT INTO Channel VALUES (NULL, 'General');
+INSERT INTO Channel VALUES (NULL, 'Christianity');
+INSERT INTO Channel VALUES (NULL, 'Islamism');
+INSERT INTO Channel VALUES (NULL, 'Hinduism');
+INSERT INTO Channel VALUES (NULL, 'Buddhism');
+INSERT INTO Channel VALUES (NULL, 'Baháí Faith');
+INSERT INTO Channel VALUES (NULL, 'Jainism');
+INSERT INTO Channel VALUES (NULL, 'Judaism');
+INSERT INTO Channel VALUES (NULL, 'Paganism');
+INSERT INTO Channel VALUES (NULL, 'Shintoism');
+INSERT INTO Channel VALUES (NULL, 'Sikhism');
+INSERT INTO Channel VALUES (NULL, 'Taoism');
+INSERT INTO Channel VALUES (NULL, 'Folk Channels');
+INSERT INTO Channel VALUES (NULL, 'IrChannel');
 
-INSERT INTO user VALUES ('Antero13', 'cdb56f6c494214b5c6cfa536daf2e42929e430e9');
-INSERT INTO user VALUES ('Pedro459669', '79e9dc154939fb9d31d59c4b1082d7b29edd6415');
-INSERT INTO user VALUES ('Andre548392', '8ef49ffc8627c1efffda5812de595253b788185e');
+INSERT INTO User VALUES ('Antero13', 'antero@gmail.com', 'cdb56f6c494214b5c6cfa536daf2e42929e430e9');
+INSERT INTO User VALUES ('Pedro459669', 'pedro@gmail.com', '79e9dc154939fb9d31d59c4b1082d7b29edd6415');
+INSERT INTO User VALUES ('Andre548392', 'andre@gmail.com', '8ef49ffc8627c1efffda5812de595253b788185e');
 
-INSERT INTO userLikesChannel VALUES(
+INSERT INTO UserLikesChannel VALUES(
     2,
     'Antero13'
 );
 
-INSERT INTO userLikesChannel VALUES(
+INSERT INTO UserLikesChannel VALUES(
     2,
     'Pedro459669'
 );
 
-INSERT INTO userLikesChannel VALUES(
+INSERT INTO UserLikesChannel VALUES(
     3,
     'Andre548392'
 );
 
-INSERT INTO publication VALUES (
-    NULL, 
-    'P',
-    'First PUBLICATION',
+INSERT INTO Publication VALUES (
+    NULL,
+    'Antero13',
     1507901651,
     'Buddhism,Christianity',
-    'Antero13',
     'Nam aliquet leo vel scelerisque sagittis. Praesent hendrerit lectus et augue condimentum, vitae dapibus elit bibendum. Quisque id sapien nec nisl commodo vulputate. Cras vehicula semper lectus. Duis a purus in velit iaculis luctus id ac justo. Mauris a lectus eu dui aliquam pretium nec a massa. Suspendisse risus metus, laoreet quis velit eu, mollis auctor tellus. Maecenas vulputate, nulla a commodo porttitor, urna arcu viverra dolor, a eleifend lectus leo a justo.',
-    'Morbi bibendum volutpat pellentesque. In bibendum est et orci semper rhoncus. Sed cursus vel orci sed malesuada. Fusce ac dictum ligula, quis hendrerit ipsum. Proin hendrerit a.
-
-Nulla commodo eu nulla ac facilisis. Donec ante lorem, tincidunt nec interdum vulputate, fringilla a urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sapien erat, suscipit a nisl sed, molestie convallis eros. Curabitur egestas massa et metus dignissim, et vestibulum libero porttitor. Sed non metus pharetra, lobortis orci a, commodo diam. Praesent a sagittis massa, quis condimentum augue. Donec id est feugiat ipsum egestas vulputate vel in dolor. Pellentesque pretium placerat lorem, sed sodales diam molestie a. Donec dictum dui ut accumsan tempor. Nam vestibulum in erat et sagittis. Donec venenatis, ante vitae tristique tristique, nisi metus aliquet.',
-    NULL,
-    0,
-    0
+    'Morbi bibendum volutpat pellentesque. In bibendum est et orci semper rhoncus. Sed cursus vel orci sed malesuada. Fusce ac dictum ligula, quis hendrerit ipsum. Proin hendrerit a. 
+    Nulla commodo eu nulla ac facilisis. Donec ante lorem, tincidunt nec interdum vulputate, fringilla a urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sapien erat, suscipit a nisl sed, molestie convallis eros. Curabitur egestas massa et metus dignissim, et vestibulum libero porttitor. Sed non metus pharetra, lobortis orci a, commodo diam. Praesent a sagittis massa, quis condimentum augue. Donec id est feugiat ipsum egestas vulputate vel in dolor. Pellentesque pretium placerat lorem, sed sodales diam molestie a. Donec dictum dui ut accumsan tempor. Nam vestibulum in erat et sagittis. Donec venenatis, ante vitae tristique tristique, nisi metus aliquet.'
 );
 
-INSERT INTO publication VALUES (
+INSERT INTO Publication VALUES (
     NULL, 
-    'P',
-    'First PUBLICATION',
+    'Pedro459669',
     1507901651,
     'Buddhism',
-    'Pedro459669',
     'TITLLEEEE',
     'TEXTTTT',
-    NULL,
-    0,
-    0
 );
 
-INSERT INTO comment VALUES(
+INSERT INTO Comment VALUES(
     NULL,
-    'C',
-    1,
     'Andre548392',
+    1,
     NULL,
     1508247632,
     'Pedro459669,Antero13',
-    'I THINK THAT THIS IS LIT AF',
-    0,
-    0
+    'I THINK THAT THIS IS LIT AF'
 );
 
-INSERT INTO voted VALUES(
+INSERT INTO Comment VALUES(
+    NULL,
+    'Andre548392',
+    NULL,
+    2,
+    1508248632,
+    'Pedro459669',
+    'HI_Comment_HERE'
+);
+
+INSERT INTO Voted VALUES(
     NULL,
     'P',
     'Antero13',
@@ -133,7 +140,7 @@ INSERT INTO voted VALUES(
     NULL
 );
 
-INSERT INTO voted VALUES(
+INSERT INTO Voted VALUES(
     NULL,
     'C',
     'Andre548392',
