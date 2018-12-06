30f46162092ee0318ef125a954cb27645939f987
EstevesAndre <up201606673@fe.up.pt>
2018-12-06 01:10:58 +0000

    deleted comment and publication functions created

---
+++ api/deleteComment.php
@@ -0,0 +1,19 @@
+<?php
+    include_once('../includes/session.php');
+    include_once('../database/db_getQueries.php');
+
+    // verifies if user is logged in
+    if (!isset($_SESSION['username']))
+        die(header('Location: login.php'));
+
+    $username = $_SESSION['username'];
+    $publication_id = $_GET['publication_id'];
+    $comment_id = $_GET['comment_id'];
+
+    if($username == 'admin' || checkIsCommentOwner($username,$comment_id))
+    {
+        deleteComment($comment_id);
+    }
+
+    header("Location: ../pages/publication.php?publication_id=$publication_id");
+?>
